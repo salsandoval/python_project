b495b2132808b7c2b0ad0944c78f2f478dfec02a
salsandoval <76705210+salsandoval@users.noreply.github.com>
2021-12-15 16:34:58 -0700

    Add files via upload

--- src/mainapp/mainapp/views.py
+++ src/mainapp/mainapp/views.py
@@ -1,10 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
-
-def home(request):
-    products = ["Cherries", "Apples", "Oranges", "Strawberries", "Pears", "Watermelon"]
-    context = {
-        'products': products,
-    }
-    return render(request, "home.html", context)
+# Create your views here.
